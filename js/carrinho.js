713a04004f6a438c5a89fd255c1f203383c40089
Roberto Dias <roberto.rhd@gmail.com>
2025-07-03 17:03:41 -0300

    separa o codigojs de abrir o  menu em outro js

--- js/carrinho.js
+++ js/carrinho.js
@@ -130,13 +130,3 @@ corpoTabela.addEventListener('input', function(evento) {
     }
   }
 });
-
-// Menu hamburguer mobile
-const botaoMenu = document.querySelector('.menu-hamburguer');
-const cabecalho = document.querySelector('header');
-
-if (botaoMenu && cabecalho) {
-  botaoMenu.addEventListener('click', () => {
-    cabecalho.classList.toggle('menu-ativo');
-  });
-}
---
+++ js/menu.js
@@ -0,0 +1,9 @@
+// Menu hamburguer mobile
+const botaoMenu = document.querySelector('.menu-hamburguer');
+const cabecalho = document.querySelector('header');
+
+if (botaoMenu && cabecalho) {
+  botaoMenu.addEventListener('click', () => {
+    cabecalho.classList.toggle('menu-ativo');
+  });
+}
